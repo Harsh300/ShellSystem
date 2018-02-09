46d96c34e6fc4f7ebb47b11d038cd38ade776cae
Harsh300 <harshan.mohanaraj@uoit.net>
2018-02-09 09:31:29 -0500

    Tokenizer and readme

--- Lab2/myshell.c
+++ Lab2/myshell.c
@@ -14,6 +14,7 @@
 #include "utility.h"
 #include "myshell.h"
 
+
 // Put macros or constants here using #define
 #define BUFFER_LEN 256
 
@@ -22,28 +23,76 @@
 // Define functions declared in myshell.h here
 
 int main(int argc, char *argv[])
-{
+{   
     // Input buffer and and commands
     char buffer[BUFFER_LEN] = { 0 };
     char command[BUFFER_LEN] = { 0 };
     char arg[BUFFER_LEN] = { 0 };
 
     // Parse the commands provided using argc and argv
-
+	 
+	
     // Perform an infinite loop getting command input from users
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
-    {
+    {	
         // Perform string tokenization to get the command and argument
 
+	char delim[] = " ";
+	char* token;
+
+	
+	for (token = strtok(buffer, delim); token; token = strtok(NULL, delim))
+	{	
+		//scanf(token,strings[i]);
+    		printf("token=%s\n", token);
+		
+	}
+		
         // Check the command and execute the operations for each command
         // cd command -- change the current directory
         if (strcmp(command, "cd") == 0)
         {
-            // your code here
-        }
+		//scanf("%s",arg);
+		 
 
+
+        }
         // other commands here...
-        
+        else if (strcmp(command, "clr") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "dir") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "environ") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "echo") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "help") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "pause") == 0)
+        {
+		scanf("%s",arg);
+
+        }
+        else if (strcmp(command, "quit") == 0)
+        {
+		scanf("%s",arg);
+
+        }
         // quit command -- exit the shell
         else if (strcmp(command, "quit") == 0)
         {
